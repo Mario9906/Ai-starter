ffcdce35fbecce691dd55570fd9373f9d98fb1cb
Mario9906 <164271234+Mario9906@users.noreply.github.com>
2025-08-25 23:05:02 +0200

    Add responsive hamburger nav

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<header class="site-header">
+  <div class="container header__inner">
+    <a href="index.php" class="brand">
+      <img src="assets/logo.svg" alt="" class="brand__logo" />
+      <span class="brand__text">Ai Starter</span>
+    </a>
+    <button class="hamburger js-menu-toggle" aria-expanded="false" aria-controls="primary-nav" aria-label="Otwórz menu">
+      <span class="hamburger__bar"></span>
+      <span class="hamburger__bar"></span>
+      <span class="hamburger__bar"></span>
+    </button>
+    <nav id="primary-nav" class="nav js-nav" aria-label="Główne">
+      <ul class="nav__list">
+        <li><a class="nav__link" href="index.php">Strona główna</a></li>
+        <li><a class="nav__link" href="portfolio.php">Portfolio</a></li>
+        <li><a class="nav__link" href="strony-www.php">Strony WWW</a></li>
+        <li><a class="nav__link" href="ulotki.php">Ulotki</a></li>
+        <li><a class="nav__link" href="social-media.php">Social Media</a></li>
+        <li><a class="nav__link" href="o-mnie.php">O mnie</a></li>
+        <li class="nav__cta"><a class="btn" href="kontakt.php">Kontakt</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
